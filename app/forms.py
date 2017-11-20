25b9bd053d7f5c5f4861386485d07b6456a9b505
zyt <zyt312074545@gmail.com>
2017-11-20 16:14:47 +0800

    complete form

--- app/forms.py
+++ app/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    openid = StringField('openid', validators=[DataRequired()])
+    openid = StringField('openid', validators=[DataRequired()])  # 前台校验
     remember_me = BooleanField('remember_me', default=False)
--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 # 激活CSRF保护
 CSRF_ENABLED = True
+
 # 激活CSRF后，建立加密的令牌，用于校验表单
 SECRET_KEY = 'something'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
